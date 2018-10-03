849431a53aad6ceee4102331c70308a830b97d59
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-03 22:20:22 +1000

    functions: Revert formatting.

--- themes/hraff/functions.php
+++ themes/hraff/functions.php
@@ -1,16 +1,16 @@
 <?php
 
 // Enable 'Featured Image' in post/page editor
-add_theme_support('post-thumbnails');
+add_theme_support( 'post-thumbnails' );
 
 // Define 'Main Menu' as a selectable location in the menu editor
-register_nav_menus(['main-menu' => 'Main Menu']);
+register_nav_menus( [ 'main-menu' => 'Main Menu' ] );
 
 // Define 'Footer Links' as a selectable location in the menu editor
-register_nav_menus(['footer-links' => 'Footer Links']);
+register_nav_menus( [ 'footer-links' => 'Footer Links' ] );
 
 // Hook the default 'excerpt_length' action and run a custom function
-add_filter('excerpt_length', 'custom_excerpt_length');
+add_filter( 'excerpt_length', 'custom_excerpt_length' );
 
 // Return a custom length of 25 words 
 function custom_excerpt_length() {
@@ -18,7 +18,7 @@ function custom_excerpt_length() {
 }
 
 // Hook the default 'excerpt_more' action and run a custom function
-add_filter('excerpt_more', 'custom_excerpt_link');
+add_filter( 'excerpt_more', 'custom_excerpt_link' );
 
 // Return HTML to create a hyperlink that links to the full post
 function custom_excerpt_link() {
