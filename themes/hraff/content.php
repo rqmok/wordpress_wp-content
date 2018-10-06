9715085676a073ed95ad13f30c2fc651112a15fa
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-06 15:23:10 +1000

    WIP: Wordpress HRAFF Theme (2)

--- themes/hraff/content.php
+++ themes/hraff/content.php
@@ -1,12 +1,14 @@
 <header>
-    <p class="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/_N2nZAThjcw" frameborder="0" allow="autoplay; encrypted-media"
-            allowfullscreen></iframe>
-    </p>
+    <?php if (get_field('youtube_video_id')) { ?>
+        <p class="video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php the_field('youtube_video_id'); ?>" frameborder="0" allow="autoplay; encrypted-media"
+                allowfullscreen></iframe>
+        </p>
+    <?php } ?>
 </header>
 <section id="content-header">
     <h1><?php the_title(); ?></h1>
-    <h2>Orban Wallace / UK / 84 Mins / English / Documentary</h2>
+    <h2><?php the_field('subtitle'); ?></h2>
 </section>
 <main class="content">
     <div id="content-text">
@@ -15,64 +17,5 @@
     <?php get_sidebar() ;?>
 </main>
 <section id="sponsor-banner">
-    <img src="images/banner.gif" alt="Sponser Advertisement">
-</section>
-<section class="cards">
-    <div class="card box-shadow">
-        <div class="card-image">
-            <img width="100%" src="images/card1-image.jpg">
-        </div>
-        <div class="card-content">
-            <div class="card-title">
-                Border Politics + Q&A
-            </div>
-            <div class="card-description">
-                Making its World Premiere at HRAFF, border politics questions whether the west has lost its moral compass adopting ideas that reject...
-            </div>
-            <div class="card-event-date">
-                Sat 12 May Melbourne
-            </div>
-            <div class="card-action-link">
-                More Info >>
-            </div>
-        </div>
-    </div>
-    <div class="card box-shadow">
-        <div class="card-image">
-            <img width="100%" src="images/card2-image.jpg">
-        </div>
-        <div class="card-content">
-            <div class="card-title">
-                Border Politics + Q&A
-            </div>
-            <div class="card-description">
-                This is Congo presents viewers a visceral look at the African conflict: the war consumes its heroes and the civilians that live among it...
-            </div>
-            <div class="card-event-date">
-                Sat 12 May Melbourne
-            </div>
-            <div class="card-action-link">
-                More Info >>
-            </div>
-        </div>
-    </div>
-    <div class="card box-shadow">
-        <div class="card-image">
-            <img width="100%" src="images/card3-image.png">
-        </div>
-        <div class="card-content">
-            <div class="card-title">
-                Border Politics + Q&A
-            </div>
-            <div class="card-description">
-                Featured Artists: Mahla Karimain, UB Lee, Hamed Rayat, Julius Bright Sackey, Helen Sheferaw and Amos Wainggai Inherent In All...
-            </div>
-            <div class="card-event-date">
-                Sat 12 May Melbourne
-            </div>
-            <div class="card-action-link">
-                More Info >>
-            </div>
-        </div>
-    </div>
+    <img src="<?php get_template_directory_uri() ?>/images/banner.gif" alt="Sponser Advertisement">
 </section>
--- themes/hraff/footer.php
+++ themes/hraff/footer.php
@@ -1,3 +1,38 @@
+        <section class="cards">
+            <?php
+                $event_posts = get_posts( [
+                    'post_type' => 'event',
+                    'numberposts' => 3,
+                    'orderby' => 'rand'
+                ] );
+
+                foreach ( $event_posts as $post ) {
+            ?>
+                <div class="card box-shadow">
+                    <div class="card-image">
+                        <img width="100%" src="<?php the_post_thumbnail_url(); ?>" />
+                    </div>
+                    <div class="card-content">
+                        <div class="card-title">
+                            <?php the_title(); ?>
+                        </div>
+                        <div class="card-description">
+                            <?php
+                                if (str_word_count(get_field('summary')) <= 25) {
+                                    the_field('summary');
+                                }
+                            ?>
+                        </div>
+                        <div class="card-event-date">
+                            <?php echo date_format(date_create(get_field('when')), 'D d M').' '.get_field('city'); ?>
+                        </div>
+                        <div class="card-action-link">
+                            <a href="<?php the_permalink(); ?>">More info >></a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </section>
         <section class="sponsor-images">
             <img src="<?php echo get_template_directory_uri() ?>/images/bankaustralia.png" alt="Bank Australia">
             <img src="<?php echo get_template_directory_uri() ?>/images/lush.png" alt="Lush">
@@ -36,5 +71,7 @@
     </article>
 
     <script src="<?php echo get_template_directory_uri() ?>/scripts/main.js"></script>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
