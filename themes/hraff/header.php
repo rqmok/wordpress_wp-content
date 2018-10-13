0ce12e1e12f9c5b750ccb4d8ffd582f4a03e85aa
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-13 21:20:29 +1100

    hraff: Change font to Montserrat.

--- themes/hraff/header.php
+++ themes/hraff/header.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php wp_title('-', true, 'right'); bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo get_stylesheet_uri(); ?>" />
     <?php wp_head(); ?>
 </head>
