48534a0099502fecac97523e7aecca4d3b8bfd05
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-13 23:54:37 +1100

    hraff: Remove mobile support and cleanup unwanted files.

--- themes/hraff/footer.php
+++ themes/hraff/footer.php
@@ -35,8 +35,6 @@
         <section class="footer-copyright">&copy <?php bloginfo('name'); ?></section>
     </article>
 
-    <script src="<?php echo get_template_directory_uri() ?>/scripts/main.js"></script>
-
     <?php wp_footer(); ?>
 </body>
 </html>
--- themes/hraff/scripts/main.js
+++
@@ -1,7 +0,0 @@
-
-// Toggles the burger menu in mobile layout
-function toggleMenu() {
-    document.querySelector('html').classList.toggle('menu-open');
-}
-
-document.querySelector('#burger-menu').addEventListener('touchend', toggleMenu);
