326788226257e412bc04c8bed52ebb2a2afdd852
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-25 22:42:34 +1100

    hraff: Add chips for filtering custom taxonomies.

--- themes/hraff/archive.php
+++ themes/hraff/archive.php
@@ -4,6 +4,28 @@
 
     <h1 class="page-heading">Events</h1>
 
+    <?php
+        $taxonomies = get_taxonomies( array( '_builtin' => false ) );
+
+        foreach( $taxonomies as $taxonomy ) {
+    ?>
+        <section class="filter-container">
+            <h2><?php echo $taxonomy ?></h2>
+            <ul class="filters">
+                <?php
+                    $terms = get_terms([
+                        'taxonomy' => $taxonomy,
+                        'hide_empty' => false,
+                    ]);
+
+                    foreach ( $terms as $term ) {
+                ?>
+                    <li><a href="<?php echo get_term_link($term, $taxonomy) ?>"><?php echo $term->name ?></a></li>
+                <?php } ?>
+            </ul>
+        </section>
+    <?php } ?>
+
     <section class="cards">
 
         <?php if (have_posts()): while (have_posts()): the_post();
