367426f21ffc9686fbe34dd51dc88bdace60f841
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-25 21:14:20 +1100

    hraff: Separate the card into a separate php file.

--- themes/hraff/archive-event.php
+++ themes/hraff/archive-event.php
@@ -6,37 +6,11 @@
 
     <section class="cards">
 
-        <?php
-            if (have_posts()): while (have_posts()): the_post();
-        ?>
-            
-            <div class="card box-shadow">
-                <div class="card-image">
-                    <a href="<?php the_permalink(); ?>"><img width="100%" src="<?php the_post_thumbnail_url(); ?>" /></a>
-                </div>
-                <div class="card-content">
-                    <div class="card-title">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </div>
-                    <div class="card-description">
-                        <?php
-                            if (str_word_count(get_field('summary')) <= 25) {
-                                the_field('summary');
-                            }
-                        ?>
-                    </div>
-                    <div class="card-event-date">
-                        <?php echo date_format(date_create(get_field('when')), 'D d M').' '.get_field('city'); ?>
-                    </div>
-                    <div class="card-action-link">
-                        <a href="<?php the_permalink(); ?>">More info >></a>
-                    </div>
-                </div>
-            </div>
+        <?php if (have_posts()): while (have_posts()): the_post();
 
-        <?php
-            endwhile; endif;
-        ?>
+            get_template_part('card', get_post_format());
+
+        endwhile; endif; ?>
 
     </section>
 
--- themes/hraff/content.php
+++ themes/hraff/content.php
@@ -32,31 +32,9 @@
 
         foreach ( $event_posts as $post ) {
             setup_postdata($post);
-    ?>
-        <div class="card box-shadow">
-            <div class="card-image">
-                <a href="<?php the_permalink(); ?>"><img width="100%" src="<?php the_post_thumbnail_url(); ?>" /></a>
-            </div>
-            <div class="card-content">
-                <div class="card-title">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </div>
-                <div class="card-description">
-                    <?php
-                        if (str_word_count(get_field('summary')) <= 25) {
-                            the_field('summary');
-                        }
-                    ?>
-                </div>
-                <div class="card-event-date">
-                    <?php echo date_format(date_create(get_field('when')), 'D d M').' '.get_field('city'); ?>
-                </div>
-                <div class="card-action-link">
-                    <a href="<?php the_permalink(); ?>">More info >></a>
-                </div>
-            </div>
-        </div>
-    <?php
+        
+            get_template_part('card', get_post_format());
+
         }
         wp_reset_postdata();
     ?>
