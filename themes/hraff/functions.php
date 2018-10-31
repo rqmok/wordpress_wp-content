4c59a812eb8410af3767f403ab5f39be25ad2d00
Zeeshan Khan <zkha0002@student.monash.edu>
2018-10-31 17:56:37 +1100

    functions: Order archive page by event date.

--- themes/hraff/functions.php
+++ themes/hraff/functions.php
@@ -25,4 +25,16 @@ function custom_excerpt_link() {
 	return ' <a href="'.get_permalink(). '">More info >></a>';
 }
 
+// Order events by their date in descending order
+// Newest events will be displayed at the top
+add_action( 'pre_get_posts', 'sort_events_by_date_desc' );
+function sort_events_by_date_desc( $query ) {
+	if ( is_archive() && $query->is_main_query() ):
+		$query->query_vars[ 'meta_key' ] = 'when';
+		$query->query_vars[ 'meta_type' ] = 'DATETIME';
+		$query->query_vars[ 'orderby' ] = 'meta_value';
+		$query->query_vars[ 'order' ] = 'DESC';
+	endif;
+}
+
 ?>
